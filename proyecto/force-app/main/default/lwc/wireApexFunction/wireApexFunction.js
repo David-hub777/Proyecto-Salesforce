ce0208394727977dc47f4ebae209711dbfe37097
David <david25042001@gmail.com>
2024-03-03 18:03:59 +0100

    Co-authored-by: Alejandro <alejandroestebangil@users.noreply.github.com> Correccion de errores

--- proyecto/force-app/main/default/lwc/wireApexFunction/wireApexFunction.js
+++ proyecto/force-app/main/default/lwc/wireApexFunction/wireApexFunction.js
@@ -1,6 +1,6 @@
 import { LightningElement, api, wire } from 'lwc';
 import { reduceErrors } from 'c/ldsUtils';
-import getContactsBornAfter from '@salesforce/apex/AccountController.getContactsBornAfter';
+import getContactsBornAfter from '@salesforce/apex/ContactController.getContactsBornAfter';
 export default class WireApexFunction extends LightningElement {
     @api minBirthDate;
     errors;
